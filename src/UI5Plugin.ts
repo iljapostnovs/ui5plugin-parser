ecc26bf5d1bee2ee36edf5e91b6792769f6e14a7
Ilja Postnovs <Ilja.Postnovs@rimibaltic.com>
2021-07-19 22:47:30 +0300

    test release

--- src/UI5Plugin.ts
+++ src/UI5Plugin.ts
@@ -6,19 +6,49 @@ import { UIClassFactory } from "./classes/UI5Classes/UIClassFactory";
 import { FileReader } from "./classes/utils/FileReader";
 import * as path from "path";
 import { ConfigHandler } from "./classes/config/ConfigHandler";
+import { IConfigHandler } from "./classes/config/IConfigHandler";
+
+interface IConstructorParams {
+	fileReader?: FileReader,
+	classFactory?: UIClassFactory,
+	configHandler?: IConfigHandler
+}
+
 export class UI5Plugin {
 	private static _instance?: UI5Plugin;
-	readonly fileReader = new FileReader();
-	readonly classFactory = new UIClassFactory();
-	readonly configHandler = new ConfigHandler();
+	private _isInitialized = false;
+	readonly configHandler: IConfigHandler = new ConfigHandler();
 
-	private constructor() {
+	get fileReader(): FileReader {
+		if (!this._isInitialized) {
+			throw new Error("Plugin must be initialized first");
+		}
+		return this.fileReader;
+	}
+	set fileReader(fileReader: FileReader) {
+		this.fileReader = fileReader;
+	}
+	get classFactory(): UIClassFactory {
+		if (!this._isInitialized) {
+			throw new Error("Plugin must be initialized first");
+		}
+		return this.classFactory;
+	}
+	set classFactory(classFactory: UIClassFactory) {
+		this.classFactory = classFactory;
+	}
+
+	private constructor(params?: IConstructorParams) {
+		this.classFactory = params?.classFactory || new UIClassFactory();
+		this.fileReader = params?.fileReader || new FileReader();
+		this.configHandler = params?.configHandler || new ConfigHandler();
+
 		return this;
 	}
 
-	public static getInstance() {
+	public static getInstance(params?: IConstructorParams) {
 		if (!UI5Plugin._instance) {
-			UI5Plugin._instance = new UI5Plugin();
+			UI5Plugin._instance = new UI5Plugin(params);
 		}
 
 		return UI5Plugin._instance;
@@ -28,6 +58,7 @@ export class UI5Plugin {
 		this.fileReader.globalStoragePath = path.join(process.cwd(), "./node_modules/ui5plugin-parser/cache");
 		try {
 			await this._preloadAllNecessaryData(wsFolders);
+			this._isInitialized = true;
 		} catch (error) {
 			console.error("Couldn't initialize plugin: " + JSON.stringify(error.message));
 		}
