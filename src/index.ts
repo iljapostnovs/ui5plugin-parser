629d68c131d21ef454e9e1c7a7f09c3bc1db6732
Ilja Postnovs <Ilja.Postnovs@rimibaltic.com>
2021-09-05 01:44:04 +0300

    fix index.html

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
 import { UI5Plugin } from "./UI5Plugin";
-const test = UI5Plugin.getInstance();
-(async function() {
-	await test.initialize();
-	const testclass = test.classFactory.getUIClass("com.test.memberrename.RenameTest");
-})();
 export = UI5Plugin;
